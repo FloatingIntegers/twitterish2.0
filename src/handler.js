39c28c00aaa9e9f62f843c09ecbd02bdafe8314f
Sofia <emmapohjalainen@gmail.com>
2016-07-14 14:37:10 +0100

    another big commit

--- src/handler.js
+++ src/handler.js
@@ -1,10 +1,13 @@
 const fs = require('fs');
-const pg = require('pg');
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/twitterish';
-const client = new pg.Client(connectionString);
+const Pool = require('pg').Pool;
+const uri = require('url');
+// const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/twitterish';
+// const client = new pg.Client(connectionString);
+const pool = new Pool({
+  host: 'localhost',
+  database: 'twitterish'
+});
 const { handleError, handleFile } = require('./handlerHelpers.js');
-
-
 
 function handler(req, res) {
   const url = req.url;
@@ -20,37 +23,33 @@ function handler(req, res) {
       if (err) handleError(res);
       else handleFile(res, data, ext);
     });
-  } else if (url === '/getTweet') {
-    client.connect((err) => {
+  } else if (url.includes('user')) {
+    const queryurl = uri.parse(url).query.split('&');
+    console.log(queryurl);
+    pool.query('SELECT id FROM users WHERE username = $1 AND password = $2;', ['bradley', 'pura'], (err, result) => {
       if (err) handleError(res);
-
-      client.query('SELECT tweets FROM tweets;', (err, result) => {
-        if (err) handleError (res);
-
-        console.log(rows.tweets);
-
-        res.writeHead(200);
-
-        res.end(rows.tweets);
-      })
-    })
+      console.log(result);
+      res.writeHead(200);
+      res.end(JSON.stringify(result));
+    });
+    res.writeHead(200);
+    res.end();
+  } else if (url === '/getTweet') {
+    pool.query('SELECT tweets FROM tweets;', (err, result) => {
+      if (err) handleError (res);
+      res.writeHead(200, { 'Content-type': 'application/json' });
+      res.end(JSON.stringify(result.rows));
+    });
   } else if (url === '/postTweet') {
     let tweetText = '';
     req.on('data', (chunk) => {
       tweetText += chunk;
     });
     req.on('end', () => {
-
-      client.connect((err) => {
-        if(err) handleError(res);
-
-        client.query('INSERT INTO tweets(user_id, tweets, timeid) values($1, $2, $3);', [1, tweetText, 'now()'], (err, result) => {
-          if (err) handleError(res);
-
-          res.writeHead(200);
-          res.end();
-          // deleted client.end function fron here because we didnt seem to need it. Leaving a comment in case something breaks.
-        });
+      pool.query('INSERT INTO tweets(user_id, tweets, timeid) values($1, $2, $3);', [1, tweetText, 'now()'], (err, result) => {
+        if (err) handleError(res);
+        res.writeHead(200);
+        res.end();
       });
     });
   } else handleError(res);
